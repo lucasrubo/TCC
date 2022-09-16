6c6cb03d552cc9980e9cc3430e626d0593acb60e
rubo <lucasrubo1@gmail.com>
2022-09-15 21:43:17 -0300

    build(css): criando o popup do login

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// index page
 app.get('/', (req, res) => {
     res.render('index');
+});
+// Sobre page
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
+// contato page
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
+// servico page
+app.get('/servico', (req, res) => {
+    res.render('servicos');
 });
 
 app.listen(port,() => {
