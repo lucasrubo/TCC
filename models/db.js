f8772c066246d3849d023fd74426c5cde9005c86
Lucas Gabriel Rubo <59654538+lucasrubo@users.noreply.github.com>
2022-10-20 11:02:15 -0300

    mudando senha pro servidor

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("tcc", "root", "123456", {
+const sequelize = new Sequelize("tcc", "root", "Tcc2022@unipcampinas", {
     host: 'localhost',
     dialect: 'mysql'
 });
